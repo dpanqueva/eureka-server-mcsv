6b73fa0aeef1516daf14f68246816378e2f2800d
Diego Alejandro Panqueva Benitez <daerb9011@gmail.com>
2024-11-18 15:26:22 +0100

    first commit

---
+++ src/main/java/com/dieva/eureka/server/mcsv/EurekaServerMcsvApplication.java
@@ -0,0 +1,15 @@
+package com.dieva.eureka.server.mcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerMcsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerMcsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dieva/eureka/server/mcsv/EurekaServerMcsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dieva.eureka.server.mcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerMcsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
